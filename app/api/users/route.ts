cab43aaba986ce614e6873470e44414db0a41cd1
Alec Faustino <alecfaustino@outlook.com>
2025-10-11 13:46:58 -0600

    api logic for all users

---
+++ app/api/users/route.ts
@@ -0,0 +1,19 @@
+// api logic for all users routes
+
+
+import { createUser, getUserById, getUsers } from "@/db/users";
+import { NextResponse, NextRequest } from "next/server";
+
+// get all users
+export async function GET() {
+  const users = await getUsers();
+  return NextResponse.json(users);
+}
+
+// create a new user
+export async function POST(request: NextRequest) {
+  const { name, email } = await request.json();
+  const newUser = await createUser(name, email);
+  return NextResponse.json(newUser);
+}
+
