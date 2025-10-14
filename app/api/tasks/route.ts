3b0d3ce074b585a59e6324ddca0bcf3feb17d26a
Alec Faustino <alecfaustino@outlook.com>
2025-10-13 18:17:58 -0600

    ping /tasks with the id from the jwt

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,21 +1,28 @@
 import { getTasksByUser, createTask } from "@/db/tasks";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse} from "next/server";
 
 // all tasks for a user
 
 export async function GET() {
-  // **** BIG TODO *** //
-  // *** IF THIS USER IS CHANGED, CHANGE THIS ID *** //
-  const id = "c5a43608-7b82-445f-934a-4b5636e2e3b3" // example id until auth is implemented // 
+  const session = await getServerSession();
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    // TODO: redirect to sign in page if not authenticated
+  }
+  // TODO REMOVE THE STRING FALLBACK AFTER TESTING
+  const id = session.user.id || "c5a43608-7b82-445f-934a-4b5636e2e3b3"; 
   const tasks = await getTasksByUser(id);
   return NextResponse.json(tasks);
 }
-
-// TODO: Revisit this route if implementing task creation by an admin/managing user's tasks
-// Maybe add a /api/user/[id]/tasks route instead
-// Example user id for postman: c5a43608-7b82-445f-934a-4b5636e2e3b3
+// create a new task
 export async function POST(request: NextRequest) {
-  const id = "c5a43608-7b82-445f-934a-4b5636e2e3b3"; // example id until auth is implemented
+  const session = await getServerSession();
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  // TODO REMOVE THE STRING FALLBACK AFTER TESTING
+  const id = session.user.id || "c5a43608-7b82-445f-934a-4b5636e2e3b3"; 
   const { title, description, dueDate, priority } = await request.json();
   const newTask = await createTask(title, id, 
      dueDate ? dueDate : undefined, description ? description : undefined, 
